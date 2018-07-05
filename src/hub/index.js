6f5a775eb5a31a6e954da55923ce1ed78bb88174
Jesse Kriss <jesse@jklabs.net>
2018-07-05 14:00:26 -0700

    making hub a class

--- src/hub/index.js
+++ src/hub/index.js
@@ -1,4 +1,3 @@
-require('dotenv').config()
 const signatures = require('sodium-signatures')
 const oyaml = require('oyaml')
 const proquint = require('proquint')
@@ -6,81 +5,92 @@ const crypto = require('crypto')
 const bs58 = require('bs58')
 const timestamp = require('../timestamp')
 
-const { HUB_ID, HUB_SECRET_KEY, HUB_PUBLIC_KEY } = process.env
-const HUB_ID_BUFFER = proquint.decode(HUB_ID)
-
-const getPublicKey = function(id) {
-  return id === HUB_ID ? HUB_PUBLIC_KEY : null
-}
-
-const createSignature = function(message) {
-  const signature = signatures.sign(new Buffer(message), bs58.decode(HUB_SECRET_KEY))
-  return signature.toString('base64')
-}
-
-const createHub = function(opts={}) {
-  return createProfile(crypto.randomBytes(2), opts)
-}
-
-const createPerson = function(opts={}) {
-  return createProfile(Buffer.concat([crypto.randomBytes(4), HUB_ID_BUFFER]), opts)
-}
-
-const createProfile = function(idBuffer, opts={}) {
-  const id = proquint.encode(idBuffer)
-
-  const keyPair = signatures.keyPair()
-  const keys = {
-    secretKey: bs58.encode(keyPair.secretKey),
-    publicKey: bs58.encode(keyPair.publicKey)
-  }
-
-  const t = timestamp.now()
-  const announceMessage = oyaml.stringify(Object.assign({t, id}, opts, { from:HUB_ID, type:'profile', t, id, publicKeys:[keys.publicKey] }))
-  const meta = {
-    route: [{ id: HUB_ID, t: timestamp.now()}],
-    signed: [
-      { id: HUB_ID, signature: createSignature(announceMessage) }
-    ]
-  }
-  return {
-    id,
-    keys,
-    message: `${announceMessage} | ${oyaml.stringify(meta)}`
-  }
-}
-
-const verifyMessage = function(messageString) {
-  const [body, metaString] = messageString.split("|").map(s => s.trim())
-  const meta = oyaml.parse(metaString)
-  const sigResults = {}
-  const bodyBuffer = new Buffer(body)
-  let anyVerified = false
-  meta.signed.forEach(({ id, signature }) => {
-    const pubKey = getPublicKey(id)
-    if (pubKey) {
-      const verificationResult = signatures.verify(bodyBuffer, Buffer.from(signature, 'base64'), bs58.decode(pubKey))
-      sigResults[id] = verificationResult
-      if (verificationResult) anyVerified = true
-    } else {
-      sigResults[id] = undefined
+class Hub {
+  constructor(config) {
+    if (!config || !config.hubId || !config.publicKey || !config.secretKey) {
+      throw new Error("Must provide hubId, publicKey, and secretKey")
     }
-  })
-  return {
-    verified: anyVerified,
-    details: sigResults
+    this.config = config
+    this.hubId = config.hubId
+    this.hubIdBuffer = proquint.decode(config.hubId)
   }
+
+  getPublicKey(id) {
+    return id === this.config.hubId ? this.config.publicKey : null
+  }
+
+  createPerson(opts={}) {
+    return this.createProfile(Buffer.concat([crypto.randomBytes(4), this.hubIdBuffer]), opts)
+  }
+
+  createHub(opts={}) {
+    return this.createProfile(crypto.randomBytes(2), opts)
+  }
+
+  createProfile(idBuffer, opts={}) {
+    const id = proquint.encode(idBuffer)
+  
+    const keyPair = signatures.keyPair()
+    const keys = {
+      secretKey: bs58.encode(keyPair.secretKey),
+      publicKey: bs58.encode(keyPair.publicKey)
+    }
+  
+    const t = timestamp.now()
+    const announceMessage = oyaml.stringify(Object.assign({t, id}, opts, { from:this.hubId, type:'profile', t, id, publicKeys:[keys.publicKey] }))
+    const meta = {
+      route: [{ id: this.hubId, t: timestamp.now()}],
+      signed: [
+        { id: this.hubId, signature: this.createSignature(announceMessage) }
+      ]
+    }
+    return {
+      id,
+      keys,
+      message: `${announceMessage} | ${oyaml.stringify(meta)}`
+    }
+  }  
+
+  createSignature(message) {
+    const signature = signatures.sign(new Buffer(message), bs58.decode(this.config.secretKey))
+    return signature.toString('base64')
+  }
+  
+  verifyMessage(messageString) {
+    const [body, metaString] = messageString.split("|").map(s => s.trim())
+    const meta = oyaml.parse(metaString)
+    const sigResults = {}
+    const bodyBuffer = new Buffer(body)
+    let anyVerified = false
+    meta.signed.forEach(({ id, signature }) => {
+      const pubKey = this.getPublicKey(id)
+      if (pubKey) {
+        const verificationResult = signatures.verify(bodyBuffer, Buffer.from(signature, 'base64'), bs58.decode(pubKey))
+        sigResults[id] = verificationResult
+        if (verificationResult) anyVerified = true
+      } else {
+        sigResults[id] = undefined
+      }
+    })
+    return {
+      verified: anyVerified,
+      details: sigResults
+    }
+  }
+    
 }
 
 if (require.main === module) {
-  // const { id, keys, message } = createHub({ geo: "San Francisco <9q8ywpy>" })
-  // const { id, keys, message } = createHub()
-  const { id, keys, message } = createPerson()
+  require('dotenv').config()
+  const hub = new Hub({ hubId: process.env.HUB_ID, publicKey: process.env.HUB_PUBLIC_KEY, secretKey: process.env.HUB_SECRET_KEY })
+  const { id, keys, message } = hub.createHub({ geo: "San Francisco <9q8ywpy>" })
+  // const { id, keys, message } = hub.createHub()
+  // const { id, keys, message } = hub.createPerson()
   console.log("new id:", id)
   console.log("secret key:", keys.secretKey)
   console.log(`announce message:\n${message}`)
 
-  const verified = verifyMessage(message)
+  const verified = hub.verifyMessage(message)
   console.log("verified?", verified)
 
 }
