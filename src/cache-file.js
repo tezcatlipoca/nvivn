82607291b89e26d4183ff7258e9167e5ecd5790b
Jesse Kriss <jesse@jklabs.net>
2018-07-10 22:47:49 -0700

    Playing around with bloom filters

--- src/cache-file.js
+++ src/cache-file.js
@@ -4,9 +4,12 @@ const oyamlStream = require('./streams/oyaml')
 const newlines = require('./streams/newlines')
 const oyamlFrontMatter = require('../src/oyaml-front-matter').fromStream
 const through2 = require('through2')
+const { CuckooFilter, BloomFilter, PartitionedBloomFilter } = require('bloom-filters')
+const zlib = require('zlib')
 
-module.exports = async function(readStream, writeStream, { key='id' }={}) {
+module.exports = async function(readStream, writeStream, { key='id', bloomFilter }={}) {
   const { frontMatter, bodyStream } = await oyamlFrontMatter(readStream)
+  let filter
   debug("loaded front matter:", frontMatter)
   const outStream = newlines()
   if (writeStream) outStream.pipe(writeStream)
@@ -16,10 +19,24 @@ module.exports = async function(readStream, writeStream, { key='id' }={}) {
   } else {
     frontMatter.keys = []
   }
+  if (bloomFilter) {
+    if (frontMatter.filter) {
+      // const jsonString = bs58.decode(msgpack.decode(frontMatter.filter))
+      // filter = CuckooFilter.fromJSON(JSON.parse(jsonString))
+      // filter = BloomFilter.fromJSON(JSON.parse(jsonString))
+      console.log("loaded filter:", filter)
+    } else {
+      const [size, fingerprintLength, bucketSize] = [100000, 3, 4]
+      filter = new CuckooFilter(size, fingerprintLength, bucketSize)
+      // const [size, errorRate] = [10000, 0.01]
+      // filter = new PartitionedBloomFilter(size, errorRate)
+    }
+  }
   const newObjs = []
   const updatedObjs = []
   const put = obj => {
     const k = obj[key]
+    if (filter) filter.add(k)
     if (keys[k]) {
       updatedObjs.push(obj)
       keys[k] = 'updated'
@@ -34,7 +51,21 @@ module.exports = async function(readStream, writeStream, { key='id' }={}) {
     delete frontMatter.keys
     outStream.write('---')
     outStream.write(oyaml.stringify(frontMatter))
-    outStream.write(oyaml.stringify({ keys: Object.keys(keys)}))
+    if (!filter) {
+      outStream.write(oyaml.stringify({ keys: Object.keys(keys)}))
+    }
+    if (filter) {
+      const jsonString = JSON.stringify(filter.saveAsJSON())
+      // const encoded = bs58.encode(msgpack.encode(jsonString))
+      // const encoded = bs58.encode(zlib.gzipSync(jsonString))
+      const binary = zlib.gzipSync(jsonString)
+      const encoded = binary.toString('base64')
+      const bloomFile = require('fs').createWriteStream('bloom.bin')
+      bloomFile.write(encoded)
+      bloomFile.end()
+      // const encoded = jsonString
+      outStream.write(oyaml.stringify({ filter: encoded }))
+    }
     outStream.write('---')
     // debug("new objs:", newObjs, "updated objs:", updatedObjs)
     updatedObjs.concat(newObjs).forEach(obj => outStream.write(oyaml.stringify(obj)))
@@ -54,8 +85,34 @@ module.exports = async function(readStream, writeStream, { key='id' }={}) {
  const getReadStream = () => {
    return bodyStream
  }
- const exists = (key) => {
-   return !!keys[key]
+ // FIXME this can only be run once, then the stream is exhausted
+ const definitelyExists = async (k) => {
+   return new Promise(resolve => {
+     if (filter) {
+       let done = false
+       bodyStream.on('data', obj => {
+         if (obj[key] === k) resolve(true)
+         done = true
+       })
+       bodyStream.on('end', () => {
+         if (done) resolve(false)
+       })
+     } else {
+       resolve(!!keys[k])
+     }
+   })
+ }
+ const maybeExists = (k) => {
+   // return !!keys[key]
+   if (filter) {
+     console.log("checking filter for", k)
+     return filter.has(k)
+   } else {
+     return !!keys[k]
+   }
+ }
+ const rate = () => {
+   return filter && filter.rate ? filter.rate() : undefined
  }
 
 return {
@@ -64,6 +121,8 @@ return {
     put,
     write,
     getReadStream,
-    exists
+    maybeExists,
+    definitelyExists,
+    rate
   }
 }
--- test/cache-files.js
+++ test/cache-files.js
@@ -4,65 +4,105 @@ const fs = require('fs')
 const path = require('path')
 const cacheFile = require('../src/cache-file')
 const oyamlFrontMatter = require('../src/oyaml-front-matter')
+const crypto = require('crypto')
 
-const writeStream = new streamBuffers.WritableStreamBuffer()
-
-tap.test('add to a sync file', async function(t) {
-  t.plan(3)
-  const file = path.join(__dirname, 'data', 'cache-file.txt')
-  const readStream = fs.createReadStream(file)
-  const c = await cacheFile(readStream, writeStream)
-  t.same(c.metadata.synced, 1531200453)
-  // add a new one
-  await c.put({ type: 'profile', id: 3, version: 1 })
-  // update an existing one
-  await c.put({ type: 'profile', id: 2, version: 2 })
-  const now = Math.floor(Date.now()/1000)
-  c.setMetadata('synced', now)
-  t.same(c.metadata.synced, now)
-  await new Promise(resolve => {
-    c.write().on('finish', async () => {
-      const oyamlString = writeStream.getContentsAsString('utf8')
-      console.log("!!!! oyaml string:", oyamlString)
-      t.same(await oyamlFrontMatter.fromString(oyamlString), {
-        frontMatter: {
-          synced: now,
-          keys: [1, 2, 3]
-        },
-        body: [
-          { id:2, type:'profile', version:2 },
-          { id:3, type:'profile', version:1 },
-          { id:1, type:'profile', version:1 }
-        ]
-      })
-      resolve()
-      t.done()
-    })
-  })
-})
-
-// tap.test("still close the write stream if there's nothing to write", async function (t) {
+// tap.test('add to a sync file', async function(t) {
+//   t.plan(3)
 //   const file = path.join(__dirname, 'data', 'cache-file.txt')
 //   const readStream = fs.createReadStream(file)
+//   const writeStream = new streamBuffers.WritableStreamBuffer()
 //   const c = await cacheFile(readStream, writeStream)
-//   t.plan(1)
-//   c.write().on('finish', () => {
-//     t.same(writeStream.getContentsAsString('utf8'), `---
-//     last-sync: 1531200453
-//     keys:[1,2,3]
-//     ---
-//     type:profile id:1 version:1
-//     type:profile id:2 version:2`)
-//     t.done()
+//   t.same(c.metadata.synced, 1531200453)
+//   // add a new one
+//   await c.put({ type: 'profile', id: 3, version: 1 })
+//   // update an existing one
+//   await c.put({ type: 'profile', id: 2, version: 2 })
+//   const now = Math.floor(Date.now()/1000)
+//   c.setMetadata('synced', now)
+//   t.same(c.metadata.synced, now)
+//   await new Promise(resolve => {
+//     c.write().on('finish', async () => {
+//       const oyamlString = writeStream.getContentsAsString('utf8')
+//       console.log("!!!! oyaml string:", oyamlString)
+//       t.same(await oyamlFrontMatter.fromString(oyamlString), {
+//         frontMatter: {
+//           synced: now,
+//           keys: [1, 2, 3]
+//         },
+//         body: [
+//           { id:2, type:'profile', version:2 },
+//           { id:3, type:'profile', version:1 },
+//           { id:1, type:'profile', version:1 }
+//         ]
+//       })
+//       resolve()
+//       t.done()
+//     })
 //   })
-
 // })
-
+//
+// // tap.test("still close the write stream if there's nothing to write", async function (t) {
+// //   const file = path.join(__dirname, 'data', 'cache-file.txt')
+// //   const readStream = fs.createReadStream(file)
+// //   const writeStream = new streamBuffers.WritableStreamBuffer()
+// //   const c = await cacheFile(readStream, writeStream)
+// //   await new Promise(resolve => {
+// //     c.write().on('finish', async () => {
+// //       const result = writeStream.getContentsAsString('utf8')
+// //       const parsed = await oyamlFrontMatter.fromString(result)
+// //       const expected = await oyamlFrontMatter.fromString(`---
+// // synced:1531200453
+// // keys:[1, 2]
+// // ---
+// // id:1 type:profile version:1
+// // id:2 type:profile version:1`)
+// //       console.log("expected:", expected)
+// //       t.same(parsed, expected)
+// //       t.done()
+// //     })
+// //   })
+// // })
+//
 // tap.test("don't die on an empty file", async function (t) {
 //   const file = path.join(__dirname, 'data', 'empty.txt')
 //   const emptyStream = fs.createReadStream(file)
+//   const writeStream = new streamBuffers.WritableStreamBuffer()
 //   const c = await cacheFile(emptyStream, writeStream)
-//   console.log(c, c.metadata)
 //   t.same(c.metadata, { keys:[] })
 //   t.done()
 // })
+
+tap.test("check if a key exists", async function(t) {
+  const file = path.join(__dirname, 'data', 'cache-file.txt')
+  const readStream = fs.createReadStream(file)
+  const c = await cacheFile(readStream)
+  t.ok(c.maybeExists(1))
+  t.ok(c.maybeExists(2))
+  t.notOk(c.maybeExists(3))
+})
+
+tap.test("check if a key exists using a bloom filter", async function(t) {
+  const file = path.join(__dirname, 'data', 'cache-file.txt')
+  const readStream = fs.createReadStream(file)
+  // const writeStream = new streamBuffers.WritableStreamBuffer()
+  const writeStream = fs.createWriteStream('tmp.txt')
+  const c = await cacheFile(readStream, writeStream, { bloomFilter: true })
+  const ids = []
+  for (let i=0; i<10; i++) {
+    const id = crypto.randomBytes(4).toString('hex')
+    ids.push(id)
+    c.put({ id, type:'profile', version:1 })
+  }
+  console.log("false positive rate is now:", c.rate())
+  t.ok(c.maybeExists(ids[0]))
+  t.ok(c.maybeExists(ids[1]))
+  t.notOk(c.maybeExists('8f985dg8'))
+  t.notOk(c.maybeExists(30000))
+  t.notOk(c.maybeExists('rip van winkle'))
+  const noReallyThough = await c.definitelyExists('8f985dg8')
+  t.notOk(noReallyThough)
+  const start = Date.now()
+  c.write()
+  const duration = Date.now() - start
+  console.log(`wrote file in ${duration} ms`)
+})
